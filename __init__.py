827627f4162358a64bc00fa2a53a468bca25be47
Yotam Barnoy <yotambarnoy@gmail.com>
2019-05-02 18:06:53 -0400

    renamed

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 
 bl_info = {
     "name": "Soldak MDM format",
-    "author": "Yotam Barnoy"
+    "author": "Yotam Barnoy",
     "blender": (2, 74, 0),
     "location": "File > Import-Export",
     "description": "Import-Export MDM, meshes, uvs, materials, textures, "
@@ -29,12 +29,12 @@ bl_info = {
     "support": 'OFFICIAL',
     "category": "Import-Export"}
 
+'''
 if "bpy" in locals():
     import importlib
-    if "import_soldak" in locals():
-        importlib.reload(import_soldak)
-    #if "export_soldak" in locals():
-        #importlib.reload(export_3ds)
+    if "import_mdm" in locals():
+        importlib.reload(import_mdm)
+'''
 
 
 import bpy
@@ -44,92 +44,38 @@ from bpy.props import (
         FloatProperty,
         StringProperty,
         )
+'''
 from bpy_extras.io_utils import (
         ImportHelper,
         ExportHelper,
         orientation_helper_factory,
         axis_conversion,
         )
+'''
 
-
-IO3DSOrientationHelper = orientation_helper_factory("IO3DSOrientationHelper", axis_forward='Y', axis_up='Z')
-
-
-class ImportSoldak(bpy.types.Operator, ImportHelper, IO3DSOrientationHelper):
+class ImportSoldak(bpy.types.Operator):
     """Import from Soldak file format (.mdm)"""
-    bl_idname = "import_scene.autodesk_Soldak"
+    bl_idname = "import_scene.soldak"
     bl_label = 'Import Soldak'
     bl_options = {'UNDO'}
 
     filename_ext = ".mdm"
+
+    if not self.filepath:
+        raise Exception("filepath not set")
+
     filter_glob = StringProperty(default="*.mdm", options={'HIDDEN'})
 
-'''
-    constrain_size = FloatProperty(
-            name="Size Constraint",
-            description="Scale the model by 10 until it reaches the "
-                        "size constraint (0 to disable)",
-            min=0.0, max=1000.0,
-            soft_min=0.0, soft_max=1000.0,
-            default=10.0,
-            )
-    use_image_search = BoolProperty(
-            name="Image Search",
-            description="Search subdirectories for any associated images "
-                        "(Warning, may be slow)",
-            default=True,
-            )
-    use_apply_transform = BoolProperty(
-            name="Apply Transform",
-            description="Workaround for object transformations "
-                        "importing incorrectly",
-            default=True,
-            )
-'''
-
     def execute(self, context):
-        from . import import_soldak
+        from . import import_mdm
 
         keywords = self.as_keywords(ignore=("axis_forward",
                                             "axis_up",
                                             "filter_glob",
                                             ))
 
-        return import_soldak.load(self, context, **keywords)
+        return import_mdm.load(self, context, **keywords)
 
-'''
-class Export3DS(bpy.types.Operator, ExportHelper, IO3DSOrientationHelper):
-    """Export to 3DS file format (.3ds)"""
-    bl_idname = "export_scene.autodesk_3ds"
-    bl_label = 'Export 3DS'
-
-    filename_ext = ".3ds"
-    filter_glob = StringProperty(
-            default="*.3ds",
-            options={'HIDDEN'},
-            )
-
-    use_selection = BoolProperty(
-            name="Selection Only",
-            description="Export selected objects only",
-            default=False,
-            )
-
-    def execute(self, context):
-        from . import export_3ds
-
-        keywords = self.as_keywords(ignore=("axis_forward",
-                                            "axis_up",
-                                            "filter_glob",
-                                            "check_existing",
-                                            ))
-        global_matrix = axis_conversion(to_forward=self.axis_forward,
-                                        to_up=self.axis_up,
-                                        ).to_4x4()
-        keywords["global_matrix"] = global_matrix
-
-        return export_3ds.save(self, context, **keywords)
-'''
 
 # Add to a menu
 #def menu_func_export(self, context):
@@ -142,16 +88,16 @@ def menu_func_import(self, context):
 
 def register():
     bpy.utils.register_module(__name__)
+    #bpy.utils.register_class(ImportSoldak)
 
     bpy.types.INFO_MT_file_import.append(menu_func_import)
-    #bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
     bpy.utils.unregister_module(__name__)
+    #bpy.utils.unregister_class(ImportSoldak)
 
     bpy.types.INFO_MT_file_import.remove(menu_func_import)
-    #bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
 if __name__ == "__main__":
     register()
